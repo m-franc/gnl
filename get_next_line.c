3621f33009bf8d70e54c207cc2ca756d2381af68
Maxime FRANC <mfranc@e2r6p3.42.fr>
2016-12-14 18:30:55 +0100

    works, but leaks and no multi fd manager

--- get_next_line.c
+++ get_next_line.c
@@ -6,76 +6,62 @@
 /*   By: mfranc <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/11/27 14:32:21 by mfranc            #+#    #+#             */
-/*   Updated: 2016/12/13 21:19:24 by mfranc           ###   ########.fr       */
+/*   Updated: 2016/12/14 18:21:36 by mfranc           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 #include <stdio.h>
 
-
-
-int		ft_strlenuntilret(char *str)
+int		save_lines(char **tmp, char *buf, char **line)
 {
-	int	i;
+	size_t	lenline;
 
-	i = 0;
-	while (str[i] && str[i] != '\n')
-		i++;
-	return (i);
-}
-/*
-   char	*ft_split_lines(char **tab, char **tmp, char *line)
-   {
-   int		i;
-   char	*newline;
-
-   i = 0;
-   while (tab[i])
-   {
-   if (ft_strcmp(tab[i], line) == 0)
-   {
-   free(line);	
-   newline = tab[i + 1];
-   }
-   i++;
-   }
-   return (newline);
-   }
-   */
-
-int		save_lines(char **tmp, char **line)
-{
+	*tmp = ft_strjoin(*tmp, buf);
 	if (ft_strchr(*tmp, '\n') != NULL)
 	{
-		*line = ft_strsub(*tmp, 0, ft_strlenuntilret(*tmp));
-		*tmp = ft_memset(*tmp, 0, ft_strlenuntilret(*tmp));
+		lenline = ft_strlen(*tmp) - ft_strlen(ft_strchr(*tmp, '\n'));
+		*line = ft_strsub(*tmp, 0, lenline);
+		*tmp = ft_strchr(*tmp, '\n') + 1;
 		return (1);
 	}
 	return (0);
 }
 
+int		ft_read(int fd, char **tmp, char **line)
+{
+	char	*buf;
+	int		ret;
+
+	if (!(buf = (char*)malloc(sizeof(char) * (BUFF_SIZE + 1))))
+		return (-1);
+	while ((ret = read(fd, buf, BUFF_SIZE)) > 0)
+	{
+		buf[ret] = '\0';
+		if (save_lines(tmp, buf, line) == 1)
+			return (1);
+	}
+	if (ret < 0)
+		return (-1);
+	else if (ret == 0)
+		return (save_lines(tmp, buf, line));
+	else
+		return (0);
+}
+
 int		get_next_line(int fd, char **line)
 {
-	char		*buf;
-	int			ret;
 	static char	*tmp;
+	int			read;
 
 	if (!fd || !line)
-		return (-1);
-	if (!(buf = (char*)malloc(sizeof(char) * (BUFF_SIZE + 1))))
 		return (-1);
 	*line = NULL;
 	if (!(tmp))
 		tmp = ft_strnew(1);
-	while ((ret = read(fd, buf, BUFF_SIZE)) > 0)
-	{
-		buf[ret] = '\0';
-		tmp = ft_strjoin(tmp, buf);
-		if (save_lines(&tmp, line) == 1)
-			return (1);
-	}
-	if (ret < 0)
+	read = ft_read(fd, &tmp, line);
+	free(tmp);
+	if (read == -1)
 		return (-1);
 	if (*line == NULL)
 		return (0);
