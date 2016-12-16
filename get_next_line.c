0d5a5a325252581d623c56c7d15534e282a70621
Maxime FRANC <mfranc@e3r11p17.42.fr>
2016-12-16 23:21:19 +0100

    prototype of test, maybe beback

--- get_next_line.c
+++ get_next_line.c
@@ -6,24 +6,28 @@
 /*   By: mfranc <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/11/27 14:32:21 by mfranc            #+#    #+#             */
-/*   Updated: 2016/12/16 20:50:17 by mfranc           ###   ########.fr       */
+/*   Updated: 2016/12/16 23:19:41 by mfranc           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 #include <stdio.h>
+
 int				save_lines(char **tmp, char **line)
 {
 	size_t		lenline;
+	char		*stop;
 
-	//ft_putendl(*line);
-	if (ft_strchr(*line, '\n') != NULL)
+	stop = ft_strchr(*tmp, '\n');
+	if (ft_strlen(stop) > 0)
 	{
-		lenline = ft_strlen(*line) - ft_strlen(ft_strchr(*line, '\n'));
-		*line = ft_strsub(*tmp, 0, lenline);
-		*tmp = ft_strchr(*line, '\n') + 1;
+		lenline = ft_strlen(*line) - ft_strlen(stop);
+		*line = ft_strsub(*line, 0, lenline);
+		*tmp = ft_strdup(stop + 1);
 		return (1);
 	}
+	else
+		return (1);
 	return (0);
 }
 
@@ -32,17 +36,15 @@ int				ft_read(int fd, char **tmp, char **line)
 	char		buf[BUFF_SIZE + 1];
 	int			ret;
 
-	while ((ret = read(fd, buf, BUFF_SIZE)) > 0)
+	printf("TMP APRES UN GNL : %s\n", *tmp);
+	while (ft_strchr(*line, '\n') != NULL)
 	{
+		ret = read(fd, buf, BUFF_SIZE);
+		if (ret < 0)
+			return (-1);
 		buf[ret] = '\0';
 		*line = ft_strjoin(*tmp, buf);
-		if (save_lines(tmp, line) == 1)
-			return (1);
 	}
-	if (ret < 0)
-		return (-1);
-	if (*line != NULL)
-		return (1);
 	return (save_lines(tmp, line));
 }
 
