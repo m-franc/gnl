cc25e76cc62d2303348df6768cd7d1c5a4150141
Maxime Franc <maxime.franc1@gmail.com>
2017-01-05 21:36:18 +0100

    correct header

--- get_next_line.h
+++ get_next_line.h
@@ -19,18 +19,17 @@
 #include <fcntl.h>
 #include "libft/libft.h"
 
-int						get_next_line(const int fd, char **line);
-int						save_lines(char *ndtmp, t_file **file, char **line);
-void					remove_file(t_file begin, int fd);
-t_file					*get_file(t_file *begin, int fd);
-t_file					lstnew(t_file **begin, int fd);
-
 typedef struct			s_file
 {
-	char				*tmp;
-	int					fd;
+	char			*tmp;
+	int			fd;
 	struct s_file		*next;
-}						t_file;
+}				t_file;
 
+int				get_next_line(const int fd, char **line);
+int				save_lines(char *ndtmp, t_file **file, char **line);
+void				remove_file(t_file **file);
+t_file				*get_file(t_file **file, int fd);
+t_file				*lstnew(t_file **begin, int fd);
 
 #endif
